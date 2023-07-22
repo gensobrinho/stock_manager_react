8e2845cefc496aa405cbd31326f21eb577f3efa9
Gabriel Estevão <gensobrinho@hotmail.com>
2023-07-22 13:51:29 -0300

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/stock_manager_react"
+  base: "https://gensobrinho.github.io/stock_manager_react/"
 })
